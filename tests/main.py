89ed45b8e1bab2e98451f57651c7d916875c5882
Noam Almog <noam.almog@cyberark.com>
2019-12-12 18:46:39 +0200

    - add pvwa api calls

--- tests/main.py
+++ tests/main.py
@@ -17,7 +17,10 @@ LINUX_OWNER = "137112412989"
 LINUX = "amzn2-ami-hvm*"
 WINDOWS_OWNER = "801119661308"
 WINDOWS = "Windows_Server-2019-English"
-PVWA_URL = "http://okok"
+PVWA_URL = "https://10.10.1.10/passwordvault"
+PVWA_IP = "10.10.1.10"
+UNIX_PLATFORM = "UnixSSHKeys"
+WINDOWS_PLATFORM = "WinServerLocal"
 
 
 def create_vm(key_pair, subnet_id, security_group_id, ami_owner, image_type, guest_os):
@@ -49,8 +52,8 @@ def create_vm(key_pair, subnet_id, security_group_id, ami_owner, image_type, gue
                               ]
                               )
     vm_info = EC2_DETAILS.describe_instances(InstanceIds=[vm[0].id])
-    vm_info = vm_info['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-    print (vm)
+    vm_info += vm_info['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+    print(vm)
     return vm_info
 
 
@@ -60,7 +63,7 @@ def create_kp():
     kp_name = kp_name.replace(":", "-")
     kp_name = kp_name.split(".")[0]
     kp_value = EC2_DETAILS.create_key_pair(KeyName=kp_name)
-    return kp_name,kp_value
+    return kp_name, kp_value
 
 
 def delete_kp(kp_name):
@@ -93,20 +96,28 @@ def main():
                                                                             linux_vm,
                                                                             PVWA_URL)
         print(vault_onbaord)
-    WindowsDDBQuery = aws_services.get_instance_data_from_dynamo_table(windows_vm)
+    WindowsDDBQuery = aws_services.get_instance_data_from_dynamo_table(windows_vm[1])
     if "on boarded" in WindowsDDBQuery:
         time.sleep(360)
         print("Windows succuess")
         vault_onbaord = pvwa_api_calls.retrieve_accountId_from_account_name(session_token, 'pcloud-test-instances-KP',
                                                                             'KpSafe5',
-                                                                            windows_vm,
+                                                                            windows_vm[1],
                                                                             PVWA_URL)
         print(vault_onbaord)
-    pvwa_api_calls.create_account_on_vault(session_token, str(windows_vm), kp[1],
-                                                                              storeParametersClass,
-                                                                              platform, windows_vm,
-                                                                              instanceId, win_account_name, safeName))
+    storeParametersClass = aws_services.StoreParameters("UxSafeCark", "WinSafeCark", "Administrator", "Noam3110!",
+                                                        PVWA_IP, "KpSafe2")
+    windows_account = pvwa_api_calls.create_account_on_vault(session_token, str(windows_vm[1]), kp[1],
+                                                             storeParametersClass,
+                                                             WINDOWS_PLATFORM, windows_vm[1],
+                                                             windows_vm[0], win_account_name, "KpSafe2")
+    linux_account = pvwa_api_calls.create_account_on_vault(session_token, str(linux_vm[1]), kp[1],
+                                                           storeParametersClass,
+                                                           UNIX_PLATFORM, linux_vm[1],
+                                                           windows_vm[0], win_account_name, "KpSafe2")
+    print(linux_account + " " + windows_account)
     delete_kp(kp[0])
     pvwa_integration.logoff_pvwa(PVWA_URL, session_token)
+
 
 main()
