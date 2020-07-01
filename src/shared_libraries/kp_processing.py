e54fa06d6e75a50b4a4d2c10828b69e984a2d35e
nalmog <noamalmog1991@gmail.com>
2020-07-01 14:41:08 +0300

    - fix path

--- src/shared_libraries/kp_processing.py
+++ src/shared_libraries/kp_processing.py
@@ -10,10 +10,10 @@ def save_key_pair(pem_key):
     logger.trace(pem_key, caller_name='save_key_pair')
     logger.info('Saving key pair to file')
     # Save pem to file
-    with open('pemValue.pem', 'w') as f:
+    with open('/tmp/pemValue.pem', 'w') as f:
         print(str(pem_key), file=f)
     #subprocess.call(f'echo {pem_file} > /tmp/pemValue.pem', shell=True)
-    subprocess.call(["chmod 777 pemValue.pem"], shell=True)
+    subprocess.call(["chmod 777 /tmp/pemValue.pem"], shell=True)
 
 
 def convert_pem_to_ppk(pem_key):
@@ -23,17 +23,15 @@ def convert_pem_to_ppk(pem_key):
     #  Uses Puttygen sent to the lambda
     save_key_pair(pem_key=pem_key)
     subprocess.call(["chmod 777 puttygen "], shell=True)
-    subprocess.check_output("cat pemValue.pem", shell=True)
-    conversion = subprocess.Popen(['./puttygen', 'pemValue.pem', '-O', 'private', '-o',
-                                   'ppkValue.ppk'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    subprocess.check_output("cat /tmp/pemValue.pem", shell=True)
+    conversion = subprocess.Popen(['puttygen', '/tmp/pemValue.pem', '-O', 'private', '-o',
+                                   '/dev/stdout'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     conversion.wait()
     conversion_result = conversion.returncode
-    Remove_Me = subprocess.Popen(['cat', 'ppkValue.ppk'], stdout=subprocess.PIPE)
-    logger.trace(conversion_result, Remove_Me.stdout.read() ,caller_name='convert_pem_to_ppk')
+    ppk_key = str(conversion.stdout.read()).replace('\'', '').replace('\\n', '\n')
     if conversion_result == 0:
-        ppk_key = subprocess.check_output("cat ppkValue.ppk", shell=True).decode("utf-8")
         logger.info("Pem key successfully converted")
-        print(ppk_key)
+        print(f'\n\n\n_________________________\n{ppk_key}')
     else:
         logger.error("Failed to convert pem key to ppk", conversion.stdout.read())
         raise Exception('Failed to convert pem')
